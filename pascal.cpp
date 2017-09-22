08de3c8af4bed76822a6be295039c9e24bceb706
Zabazarnykh Varya <kemma.kart@mail.ru>
2017-09-22 15:31:29 +0300

    Add calculation of Pascal

--- pascal.cpp
+++ pascal.cpp
@@ -1,18 +1,27 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-    // TODO: написать вывод треугольника Паскаля с использованием vector.
-    //
-    // Вид треугольника:
-    // 1
-    // 1 1
-    // 1 2 1
-    // 1 3 3 1
-    // 1 4 6 4 1
-    // ...
+    int n, i, t;
+    cin >> n;
+    vector<int> v;
+    v.push_back(1);
+    cout << v[0]<< endl;
+    for (i = 2; i<=n; ++i){
+	vector<int> k;
+	k.push_back(1);
+	for(t = 0; t < i - 2; ++t){
+	    k.push_back(v[t]+v[t+1]);
+	}
+	k.push_back(1);
+	for(t = 0; t < i; ++t){
+	    cout << k[t]<< " ";
+	    v = k;
+	}
+	cout << endl;
+    }
     return 0;
 }
-
